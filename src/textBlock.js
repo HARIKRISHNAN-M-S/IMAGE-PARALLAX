6e718a4be4036680af223580ed8399821d8921af
HARIKRISHNAN-M-S <harinaduvil26@gmail.com>
2023-11-24 01:38:26 +0530

    image

---
+++ src/textBlock.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title"> JUST SCROLL UP AND DOWN </h1>
+                <p id="textblock-content">
+              
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis dictum lectus, quis ullamcorper leo dignissim tincidunt. Quisque facilisis blandit nisl, vitae mattis nisl. Vivamus at vestibulum mauris. Donec convallis purus in nulla cursus eleifend. Nullam eu dapibus ligula, et viverra orci. Nam porta laoreet mi, id dictum ex ornare sit amet. Nulla posuere odio mi, vel consequat velit feugiat quis.
+
+Nulla vel ligula condimentum, imperdiet urna quis, eleifend justo. Ut tincidunt pellentesque dignissim. Morbi sagittis lacus nec vulputate auctor. Nulla semper eu felis at sodales. Nulla sit amet eros id libero tristique vehicula. Donec vitae fermentum nulla. Sed in lacus nisi. Nam condimentum iaculis auctor. Phasellus suscipit nulla a ultrices rhoncus. In tincidunt dui sem, sit amet rhoncus lacus viverra a. Morbi id tortor consequat, malesuada lacus vel, scelerisque dui. Proin auctor semper mattis. Suspendisse pretium dictum faucibus. Donec vitae nisl nec urna porttitor auctor. Pellentesque in arcu sagittis tortor mattis ultricies. Cras mollis auctor justo, at aliquam magna tempus quis.
+
+In ullamcorper, mi id fringilla auctor, tellus ex consequat nibh, tincidunt ornare nisl leo scelerisque dolor. Nulla facilisi. Morbi volutpat commodo rutrum. Proin tempus egestas augue ut elementum. In vitae varius diam, in pharetra dui. Donec at lectus ac turpis interdum imperdiet pulvinar vitae tellus. Nunc rhoncus libero lorem, id commodo dui ornare ac. Aenean facilisis metus vitae ex convallis fermentum. Donec laoreet lorem quis erat viverra, ut elementum justo pharetra. Morbi lacinia dolor justo, efficitur blandit sem malesuada eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer varius ipsum velit, vel tempus tellus consectetur in. Nunc ut finibus orci.
+
+Sed interdum euismod lacus, non rhoncus elit tempor sit amet. Donec ut urna sed urna dapibus condimentum. Quisque congue neque dignissim vestibulum euismod. Mauris vitae enim sed risus sodales vulputate. Proin ornare auctor dictum. Curabitur euismod semper nulla, vitae porttitor odio pulvinar nec. Fusce eu augue ac felis lacinia efficitur. Ut ornare scelerisque mattis. Proin hendrerit molestie diam sed porta. Etiam vulputate nisl ac risus viverra porta.
+
+Fusce dictum odio tortor, sit amet accumsan magna elementum sed. Nullam sit amet velit quis mi pulvinar rutrum ac vitae nunc. Vivamus vel volutpat velit. Nunc eleifend aliquam ipsum, a eleifend orci. Curabitur egestas commodo dolor ac rutrum. Morbi scelerisque rutrum lectus vel aliquet. Vestibulum ultrices dui quis eros viverra lobortis. Maecenas facilisis lacus vel blandit congue. Etiam dignissim sapien quis ultrices viverra. Sed commodo tristique elit eget tempor. Quisque et mi convallis, malesuada diam nec, congue dolor. Integer aliquam in odio sed congue. Donec euismod sempe
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default TextBlock;
